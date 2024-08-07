a73aa72193500ad8863d1933a9f213083d64af93
adarsh249 <adarshpatel249@gmail.com>
2024-08-07 15:57:21 -0700

    Added CSS to inforgraphics on mid-landing page, added basic footer

--- campus-eats/src/app/components/Footer.tsx
+++ campus-eats/src/app/components/Footer.tsx
@@ -1,9 +1,37 @@
 'use client'
 
-export default function Header() {
+export default function Footer() {
   return (
-    <footer >
-        <p>&copy; 2024 Campus Eats</p>
-    </footer>
+      <footer className="bg-gray-800 text-white py-4 flex flex-row flex-wrap justify-around">
+              <div className="flex flex-wrap content-center justify-evenly">
+                  <div className="w-1/3 mb-6">
+                      <h3 className="text-lg font-semibold mb-4">About Us</h3>
+                      <p className="text-gray-400">
+                          I simply made this site to learn how Next.js, React, Tailwind CSS, Typescript, and much more all work together. I've primarly worked
+                          on vanilla HTML, CSS, and JS so this has been an amazing learning experience. I hope you enjoy the site and let me know what improvements to make!
+                      </p>
+                  </div>
+                  <div className="w-1/3 mb-6 flex flex-col content-center">
+                      <h3 className="text-lg font-semibold mb-4 text-center">Quick Links</h3>
+                      <ul className="list-none text-center">
+                          <li>
+                              <a href="/" className="text-gray-300 hover:text-white text-center">Home</a>
+                          </li>
+                          <li>
+                              <a href="/release-notes" className="text-gray-300 hover:text-white">Release Notes</a>
+                          </li>
+                          <li>
+                              <a href="/contact" className="text-gray-300 hover:text-white">Contact</a>
+                          </li>
+                          <li>
+                              <a href="/register" className="text-gray-300 hover:text-white">Sign Up</a>
+                          </li>
+                      </ul>
+                  </div>
+              </div>
+              <div className="text-center text-gray-400 mt-8">
+                  <p>&copy; {new Date().getFullYear()} Campus Eats. All rights reserved.</p>
+              </div>
+      </footer>
   );
 }
--- campus-eats/src/app/components/landing/Features.tsx
+++ campus-eats/src/app/components/landing/Features.tsx
@@ -2,16 +2,23 @@
 
 export default function Features() {
     return (
-        <section className="flex justify-center align-center flex-col">
-            <h2>Add to Campus Eats!</h2>
-            <img src="assets/svgs/rmp-edit.svg" alt="svg of a student with a pencil" />
-            <p>Help us grow our database by adding your school's dining options!</p>
-            <img src="assets/svgs/rmp-anon.svg" alt="svg of a student with a mask" />
-            <p>Keep your identity anonymous while contributing.</p>
-            <img src="assets/svgs/rmp-like.svg" alt="svg of a student with a thumbs up" />
-            <p>Rate and review your favorite dining options.</p>
-            <a href="/register">Sign Up!</a>
-            
+        <section className="flex flex-col justify-center items-center">
+            <h2 className="mt-10 mb-5">Add to Campus Eats!</h2>
+            <div className="flex flex-row flex-wrap justify-center items-center">
+                <figure className="w-full sm:w-6/12 md:w-4/12 p-2 flex flex-col items-center">
+                    <img src="assets/svgs/rmp-edit.svg" alt="svg of a student with a pencil" className="min-h-56"/>
+                    <figcaption className="text-center">Add all your school's dining options</figcaption>
+                </figure>
+                <figure className="w-full sm:w-6/12 md:w-4/12 p-2 flex flex-col items-center">
+                    <img src="assets/svgs/rmp-anon.svg" alt="svg of a student with a mask" className="min-h-56"/>
+                    <figcaption className="text-center">Keep your identity anonymous while contributing</figcaption>
+                </figure>
+                <figure className="w-full sm:w-6/12 md:w-4/12 p-2 flex flex-col items-center">
+                    <img src="assets/svgs/rmp-like.svg" alt="svg of a student with a thumbs up" className="min-h-56"/>
+                    <figcaption className="text-center">Rate and review your favorite dining options</figcaption>
+                </figure>
+            </div> 
+            <a href="/register" className="my-7 border-2 border-black rounded-xl px-5 py-1 bg-sky-500">Sign Up!</a>
         </section>
     )
 }
